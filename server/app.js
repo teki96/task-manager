b41b971cbf681bc221c50e5af11adf86dacd9151
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-03 18:50:28 +0200

    Utils and db connection

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+module.exports = app
