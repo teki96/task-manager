c6a67277d2c0ac8b65dee767417f85936db2c7eb
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-03 20:06:03 +0200

    Changes to users model

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  name: String,
+  passwordHash: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
