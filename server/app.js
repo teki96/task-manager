d3e0c4ddce21d6a662c668150d1bc3a085cbe887
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-03 20:02:30 +0200

    Added middlewareusage and users router to app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -21,5 +23,11 @@ mongoose
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+app.use('/api/users', usersRouter)
 
 module.exports = app
