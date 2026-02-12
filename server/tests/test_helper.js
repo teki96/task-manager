39e125b5df3b449918fcdf38ddae55cc6ac0e474
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-12 22:03:00 +0200

    add helper for retrieving tasks in tests

--- server/tests/test_helper.js
+++ server/tests/test_helper.js
@@ -1,10 +1,17 @@
 const User = require('../models/user')
+const Task = require('../models/task')
 
 const usersInDb = async () => {
   const users = await User.find({})
   return users.map((u) => u.toJSON())
 }
 
+const tasksInDb = async () => {
+  const tasks = await Task.find({})
+  return tasks.map((t) => t.toJSON())
+}
+
 module.exports = {
   usersInDb,
+  tasksInDb,
 }
