2a0bd9dabe3fa803dbbb6023b2592cf6c9f65004
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-14 14:51:30 +0200

    add authentication service

---
+++ client/src/services/authService.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export const authService = {
+  register: (userData) => {
+    return api.post('/api/users', userData)
+  },
+
+  login: (username, password) => {
+    return api.post('/api/login', { username, password })
+  },
+
+  logout: () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+  },
+
+  getCurrentUser: () => {
+    const user = localStorage.getItem('user')
+    return user ? JSON.parse(user) : null
+  },
+
+  getToken: () => {
+    return localStorage.getItem('token')
+  },
+
+  isAuthenticated: () => {
+    return !!localStorage.getItem('token')
+  },
+}
+
+export default api
