b86cf07698910ea9dc0e51478a590c186c5f5dee
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-16 00:04:35 +0200

    styling changes for login/register form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -86,7 +86,7 @@ export const LoginForm = () => {
             <Button
               type="submit"
               disabled={isLoading}
-              className="w-full bg-blue-600 hover:bg-blue-700 disabled:opacity-50"
+              className="w-full h-10 bg-blue-600 hover:bg-blue-700 disabled:opacity-50"
             >
               {isLoading ? 'Logging in...' : 'Login'}
             </Button>
--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -149,7 +149,7 @@ export const RegisterForm = () => {
             <Button
               type="submit"
               disabled={isLoading}
-              className="w-full bg-blue-600 hover:bg-blue-700 disabled:opacity-50"
+              className="w-full h-10 bg-blue-600 hover:bg-blue-700 disabled:opacity-50"
             >
               {isLoading ? 'Registering...' : 'Register'}
             </Button>
