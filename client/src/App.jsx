20cff02d79f724ccf62f6232115d013491e781cf
teki96 <teemu.t.kinnunen@tuni.fi>
2026-02-16 17:08:46 +0200

    add task provider to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,27 +10,30 @@ import { RegisterPage } from './pages/RegisterPage'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { DashboardPage } from './pages/DashboardPage'
 import { Navigation } from './components/Navigation'
+import { TaskProvider } from '../../client_files/client/src/context/TaskContext'
 
 function App() {
   return (
     <>
       <Router>
         <AuthProvider>
-          <Navigation />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
-          </Routes>
+          <TaskProvider>
+            <Navigation />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <DashboardPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            </Routes>
+          </TaskProvider>
         </AuthProvider>
       </Router>
     </>
